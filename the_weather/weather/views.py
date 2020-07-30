ea2a69f194dd5d52ebd2d31cc0abc975798cf495
sd8917 <sudhanshuraj8917@gmail.com>
2020-07-31 00:28:00 +0530

    initial version of weatherapp

---
+++ the_weather/weather/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+# def index(request):
+#     url = 'http://api.openweathermap.org/data/2.5/weather?q=las%20vegas&units=imperial&appid=35e3987b62cdffe11371dc374e53b505'
+#     city = 'West Bengal'
+#     city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+#     weather = {
+#         'city' : city,
+#         'temperature' : city_weather['main']['temp'],
+#         'description' : city_weather['weather'][0]['description'],
+#         'icon' : city_weather['weather'][0]['icon']
+#     }
+
+#     form = CityForm()
+
+#     weather_data = []
+
+#     for city in cities:
+
+#         city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+#         weather = {
+#             'city' : city,
+#             'temperature' : city_weather['main']['temp'],
+#             'description' : city_weather['weather'][0]['description'],
+#             'icon' : city_weather['weather'][0]['icon']
+#         }
+
+#         weather_data.append(weather) #add the data for the current city into our list
+
+#     context = {'weather_data' : weather_data}, 'form':form } 
+
+
+#     context = {'weather' : weather}
+
+#    # print(context["weather"]["icon"])
+
+#     return render(request, 'weather/index.html',context) #returns the index.html template
+
+
+def index(request):
+    cities = City.objects.all() #return all the cities in the database
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=35e3987b62cdffe11371dc374e53b505'
+   
+    if request.method == 'POST': # only true if form is submitted
+        form = CityForm(request.POST) # add actual request data to form for processing
+        form.save() # will validate and save if validate
+
+    form = CityForm()
+
+
+    weather_data = []
+
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon']
+        }
+
+        weather_data.append(weather) #add the data for the current city into our list
+
+    context = {'weather_data' : weather_data, 'form' : form}
+    
+    return render(request, 'weather/index.html',context) #returns the index.html template
